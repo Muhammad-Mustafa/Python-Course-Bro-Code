1ad1b2e217996ba420c7d3fee41036be8a85331f
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2024-10-29 19:21:19 +0500

    chapter TypeCasting is completed

---
+++ 4. type casting/type_casting.py
@@ -0,0 +1,16 @@
+# Type casting is when you convert the data type of a variable to another data type
+
+x, y, z = 5, 65.8, "5"
+print("The Type of X is " +str(type(x)))
+print("The Type of Y is " +str(type(y)))
+print("The Type of Z is " +str(type(z)))
+
+x, y, z = str(5), str(65.8), int("5")
+
+print("The value of X is " +str(x))
+print("The value of Y is " +str(y))
+print("The value of Z is " +str(z))
+
+print("The Type of X is " +str(type(x)))
+print("The Type of Y is " +str(type(y)))
+print("The Type of Z is " +str(type(z)))
