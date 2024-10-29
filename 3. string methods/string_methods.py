4a5153d9d96270391d6ea810c7f898b1fae416e9
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2024-10-29 19:01:10 +0500

    string methods chapter compelte

---
+++ 3. string methods/string_methods.py
@@ -0,0 +1,12 @@
+name= "Muhammad Mustafa"
+
+print(len(name))
+print(name.find("M"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha()) #Fasle because we have space in the name, only return True when we have only letters 
+print(name.count("m"))
+print(name.replace("m", "t"))
+print(name * 3)
