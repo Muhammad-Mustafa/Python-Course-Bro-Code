e898388eb8d035cfb4a7e5afaad74e8e91af4f3f
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2024-10-30 19:40:03 +0500

    if else statements in python

---
+++ 8. if statements/if_else.py
@@ -0,0 +1,13 @@
+# if statement =  a block of code only runs when its TURE
+#elif = if we have more conditions other then if statements we use elif 
+# else statement = when the IF statement is false then else stetement will run 
+
+age = int(input("What is your age : "))
+if age == 100:
+  print("you are a century old !!")
+elif age > 18:
+  print("You are an adult now!!")
+elif age <= 18 and age >0:
+  print("You are still a kid buddy !")
+else:
+  print("you are not even born yet ;(")
