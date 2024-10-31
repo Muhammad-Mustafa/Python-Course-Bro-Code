729d85658eb011b21033fcbc1e44dafda3955d2a
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2024-10-31 23:40:33 +0500

    2-D list

---
+++ 15. 2D list/2D_list.py
@@ -0,0 +1,10 @@
+# 2-D lists are 2 dimentions list 
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice-cream"]
+
+food = [drinks, dinner, dessert]
+print(food)
+print(food[0])
+print(food[0][2])
