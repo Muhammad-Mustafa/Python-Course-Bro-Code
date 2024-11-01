bdf044f536083b18aef1c3c39effbc6eb33ae32c
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2024-11-02 03:21:19 +0500

    date time module in python

---
+++ 19. date-time/date_time.py
@@ -0,0 +1,12 @@
+import datetime
+
+time = datetime.datetime.now()
+
+print(time)
+print(time.year)
+print(time.date)
+print(time.hour)
+print(time.minute)
+print(time.second)
+print(time.month)
+print(time.day)
